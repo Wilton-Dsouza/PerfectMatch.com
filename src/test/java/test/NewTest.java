4cb904800129a09a222068a667b17535270692e1
Wilton Santhosh D Souza <58103652+Wilton-Dsouza@users.noreply.github.com>
2022-06-03 13:05:47 +0530

    Update NewTest.java

--- src/test/java/test/NewTest.java
+++ src/test/java/test/NewTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.io.File;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -7,14 +9,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.io.File;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class NewTest {
+//	WebDriver driver = new ChromeDriver();
   @Test
-  public void f() throws InterruptedException {
+  public void AddProfile() throws InterruptedException {
 	  WebDriverManager.chromedriver().setup();
 		JavascriptExecutor js;
 		// creating driver instance
@@ -47,11 +51,11 @@ public class NewTest {
 		name1.sendKeys("Tester");
 		password1.sendKeys("123456@");
 		login.click();
-// 		WebElement pricing = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a"));
-// 		pricing.click();
-// 		Thread.sleep(5000);
-// 		WebElement assertLabel = driver.findElement(By.xpath("//*[@id=\"pricing\"]/h2"));
-// 		Assert.assertEquals("A Plan for Every Perfect Couple !", assertLabel.getText());
+		WebElement pricing = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a"));
+		pricing.click();
+		Thread.sleep(5000);
+		WebElement assertLabel = driver.findElement(By.xpath("//*[@id=\"pricing\"]/h2"));
+		Assert.assertEquals("A Plan for Every Perfect Couple !", assertLabel.getText());
 		WebElement subscribeFree = driver.findElement(By.xpath("//*[@id=\"pricing\"]/div/div[1]/div/div[2]/a"));
 		js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].scrollIntoView(true);",subscribeFree);
@@ -65,22 +69,23 @@ public class NewTest {
 		bioInSubscribe.sendKeys("teacher");
 		interestsInSubscribe.sendKeys("Chemistry");
 		Thread.sleep(5000);
+		String path = new File("images\\hiesenberg.jpg").getAbsolutePath();
 		WebElement imageInSubscribe = driver.findElement(By.xpath("//*[@id=\"image\"]"));
 		//js.executeScript("driver.findElement(By.xpath(\"//*[@id=\\\"image\\\"]\")).value="+path);
-	  	String path = new File("web\\images\\couple.jpg").getAbsolutePath();
+		//imageInSubscribe.sendKeys("C:\\Users\\I526698\\eclipse-workspace\\Devops\\images\\hiesenberg.jpg");
 		imageInSubscribe.sendKeys(path);
 		Thread.sleep(5000);
-		
+	
 		WebElement subscribeInBio = driver.findElement(By.xpath("/html/body/div[1]/form/div[5]/input[1]"));
 		js.executeScript("arguments[0].scrollIntoView(true);",subscribeInBio);
 		subscribeInBio.click();
 		Alert al = driver.switchTo().alert();
 	    // click on OK to accept with accept()
 	    al.accept();
-	    
-// 	    WebElement profileBtn = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[4]/a"));
-// 	    profileBtn.click();	    
-// 	    Thread.sleep(4000);
+	    Thread.sleep(4000);
+//	    WebElement profileBtn = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[4]/a"));
+//	    profileBtn.click();	    
+	   
 	   
 		
 		//WebElement  lastUploaded = driver.findElement(By.xpath("/html/body/form/font/font/font/font/font/section"));
@@ -92,24 +97,71 @@ public class NewTest {
 		
 		driver.quit();
   }
-	@Test
-public void test2() throws InterruptedException {
-WebDriverManager.chromedriver().setup();
-JavascriptExecutor js;
-WebDriver driver = new ChromeDriver();
-int number = 0;
-number++;
-// driver.get("https://www.instagram.com/");
-driver.get("https://matrimony-perfectmatch.000webhostapp.com/login.php");
-Thread.sleep(5000);
-WebElement name1 = driver.findElement(By.name("username"));
-WebElement password1 = driver.findElement(By.name("password"));
-WebElement login = driver.findElement(By.xpath("/html/body/div[1]/form/div[3]/input"));
-name1.sendKeys("Tester");
-password1.sendKeys("123456@");
-login.click();
-driver.quit();
-
+  
+  
+  @Test
+  public void Navigation() throws InterruptedException {
+	  WebDriverManager.chromedriver().setup();
+		JavascriptExecutor js;
+		WebDriver driver = new ChromeDriver();
+		js = (JavascriptExecutor) driver;
+		driver.get("https://matrimony-perfectmatch.000webhostapp.com/login.php");
+		Thread.sleep(5000);
+		WebElement name1 = driver.findElement(By.name("username"));
+		WebElement password1 = driver.findElement(By.name("password"));
+		WebElement login = driver.findElement(By.xpath("/html/body/div[1]/form/div[3]/input"));
+		name1.sendKeys("Tester");
+		password1.sendKeys("123456@");
+		login.click();
+		WebElement contactTab = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a"));
+		contactTab.click();
+		Thread.sleep(2000);
+		js.executeScript("arguments[0].scrollIntoView(true);",contactTab);
+		Thread.sleep(2000);
+		WebElement pricingTab = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a"));
+		pricingTab.click();
+		Thread.sleep(2000);
+		js.executeScript("arguments[0].scrollIntoView(true);",pricingTab);
+		Thread.sleep(2000);
+		WebElement downloadTab = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a"));
+		js.executeScript("arguments[0].scrollIntoView(true);",downloadTab);
+		Thread.sleep(2000);
+		downloadTab.click();
+		
+		driver.quit();
+	  
+  }
+  
+  @Test
+  public void ViewProfile() throws InterruptedException {
+	  WebDriverManager.chromedriver().setup();
+		JavascriptExecutor js;
+		WebDriver driver = new ChromeDriver();
+		js = (JavascriptExecutor) driver;
+		driver.get("https://matrimony-perfectmatch.000webhostapp.com/login.php");
+		Thread.sleep(5000);
+		WebElement name1 = driver.findElement(By.name("username"));
+		WebElement password1 = driver.findElement(By.name("password"));
+		WebElement login = driver.findElement(By.xpath("/html/body/div[1]/form/div[3]/input"));
+		name1.sendKeys("Tester");
+		password1.sendKeys("123456@");
+		login.click();
+		WebElement profileTab = driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[4]/a"));
+		profileTab.click();
+		driver.quit();
+		
+  }
+  
+  
+  
+  @BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Starting Test On Chrome Browser");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		// driver.close();
+		System.out.println("Finished Test On Chrome Browser");
+	}
 }
-}
-
